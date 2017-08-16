f5893d331baf3d136f2d19b62096c827400199b9
bigspiderfee <popgimmick.study@gmail.com>
2017-08-16 11:19:53 +0000

    kadai favorite first

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :favoritings, through: :favorites, source: :micropost
+  #has_many :reverses_of_favorite, class_name: 'Favolite', foreign_key: 'micropost_id'
+  #has_many :favorite_users, through: :reverses_of_favorite, source: :user
 
   def follow(other_user)
     unless self == other_user
@@ -29,5 +33,23 @@ class User < ApplicationRecord
 
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+
+  def favorite(other_favorite)
+    unless self == other_favorite
+      self.favorites.find_or_create_by(micropost_id: other_favorite.id)
+    end
+  end
+
+  def unfavorite(other_favorite)
+    unless self == other_favorite
+      favorite = self.favorites.find_by(micropost_id: other_favorite.id)
+      favorite.destroy if favorite
+    end
+  end
+    
+  def favoriting?(micropost)
+    self.favoritings.include?(micropost)
+    # binding.pry
   end
 end
